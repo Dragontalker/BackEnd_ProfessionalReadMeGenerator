2442d79aff072d903f4da8b16a92989617b15f4b
Richard Yang <richard.yang.tong@gmail.com>
2021-02-10 10:13:38 -0500

    Removed the instruction comments.

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-// TODO: Include packages needed for this application
 const fs = require("fs");
 const util = require("util");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// Creating writeFile function using promises instead of a callback function.
 const writeFileAsync = util.promisify(fs.writeFile);
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
 const userInput = () => {
     return inquirer.prompt([
@@ -20,7 +16,6 @@ const userInput = () => {
     ]);
 };
 
-// TODO: Create a function to initialize app
 const init = () => {
     userInput()
       .then((answers) => writeFileAsync('./results/result.md', generateMarkdown(answers)))
@@ -28,5 +23,4 @@ const init = () => {
       .catch((err) => console.error(err));
 };
 
-// Function call to initialize app
 init();
